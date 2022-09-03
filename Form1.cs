a8ab3434c3149848ee8fcf12d965c260c2f1b744
Necrodever <necrodever@gmail.com>
2022-09-03 16:03:17 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTestWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Environment.GetCommandLineArgs().Where(x => x.EndsWith("key")).ToList().ForEach(x =>
+            {
+                MessageBox.Show("test");
+                Application.Exit();
+                Environment.Exit(0); // Выйдет, а остальное вызовет баг с автонажатием кнопки да
+                Close();
+                return;
+            });
+
+            var path = @"Directory\background\shell\Useless ARW\command";
+
+            using (var key = Registry.ClassesRoot.OpenSubKey(path, true))
+            {
+                if (key == null)
+                {
+                    if (MessageBox.Show("Хотите добавить?", Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Registry.ClassesRoot.CreateSubKey(path).SetValue("", $@"{Assembly.GetEntryAssembly().Location} /key");
+                    }
+                }
+                else
+                {
+                    if (MessageBox.Show("Хотите удалить?", Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Registry.ClassesRoot.DeleteSubKeyTree(@"Directory\background\shell\Useless ARW\", false);
+                    }
+                }
+            }
+            Application.Exit();
+            //Close();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("BugTestWF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BugTestWF")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("26812f79-ee58-4780-a528-1fbf8b4d68f7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
